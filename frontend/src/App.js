cb0d38d31ccfb70f4534b1f88ff3a9e306888d7f
nokkov <arkady-chesnokof@yandex.ru>
2024-01-29 08:27:31 +0300

    feat: add simple form; TODO: do styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,14 @@ function App() {
           <img src="ULTIMATE-URL-SHORTENER.gif" alt="logo" />
         </div>
       </div>
+      <div className="input-fields">
+        <form id="urlForm">
+          <input type="text" id="longUrlField" required></input>
+          <input type="text" id="shortUrlField" readOnly></input>
+          <button type="button" onClick="submitForm()">SEND NUDES!</button>
+        </form>  
+      </div>
+      <script defer src="./handler.js"></script>
     </div>
   );
 }

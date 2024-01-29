874b5e34515546d583c52f9fa1f103bf77dbb499
nokkov <arkady-chesnokof@yandex.ru>
2024-01-29 07:07:21 +0300

    feat: init react app & add logo + background

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="logo">
+          <img src="ULTIMATE-URL-SHORTENER.gif" alt="logo" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;

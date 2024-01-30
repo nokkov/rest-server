117d643dab61d5f46dccb4b73f751fd9bd11c612
nokkov <arkady-chesnokof@yandex.ru>
2024-01-30 10:50:34 +0300

    feat: complete submitForm()

--- frontend/src/handler.js
+++ frontend/src/handler.js
@@ -1,5 +1,18 @@
 function submitForm() {
-    console.log('dsa')
+    let longUrl = document.getElementById('longUrlField').value; 
+
+    fetch('localhost:8080/save-url', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+
+        body: JSON.stringify({"LONG_URL":longUrl})
+    })
+
+    .then(response => response.json())
+    .then(response => console.log(JSON.stringify(response)))
 }
 
 export default submitForm;
